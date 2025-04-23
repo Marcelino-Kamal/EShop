c59dfd56980b86ee13822ddd475ea86bc94928ef
Marcelino <marconaoum@gmail.com>
2025-04-24 00:42:16 +0200

    Service

---
+++ CookiesShop/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using CookiesShop.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace CookiesShop.Services
+{
+    public static class UserService
+    {
+
+       
+        public static bool IsAuthenticated(Users u,string hashedPassword, string enteredPassword) {
+
+            return new PasswordHasher<Users>().HashPassword(u,enteredPassword) == hashedPassword;
+        }
+        public static string HashMyPass(Users u) { 
+            
+            return new PasswordHasher<Users>().HashPassword(u,u.Password);
+        }
+       
+
+        
+    }
+}
