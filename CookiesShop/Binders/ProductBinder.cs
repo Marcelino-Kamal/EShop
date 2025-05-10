52646130c2373c4c3078b0e3acd87b3306c49984
Marcelino <marconaoum@gmail.com>
2025-05-10 23:24:04 +0300

    Binders

--- CookiesShop/Binders/ProductBinder.cs
+++ CookiesShop/Binders/ProductBinder.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using CookiesShop.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace CookiesShop.Binders
 {
@@ -6,11 +7,25 @@ namespace CookiesShop.Binders
     {
         public Task BindModelAsync(ModelBindingContext bindingContext)
         {
+            string Name = bindingContext.HttpContext.Request.Form["name"];
+            string Desc = bindingContext.HttpContext.Request.Form["desc"];
+            //string imgUrl = bindingContext.HttpContext.Request.Form["imgUrl"];
+            decimal price = Convert.ToDecimal(bindingContext.HttpContext.Request.Form["price"]);
+            bool inStock = Convert.ToBoolean(bindingContext.HttpContext.Request.Form["inStock"]);
+            int CatID = Convert.ToInt32(bindingContext.HttpContext.Request.Form["CatID"]);
+
+            var product = new Products() {
+            
+                name = Name,
+                description = Desc,
+                price = price,
+                inStock = inStock,
+                CategoryID = CatID,
+            
+            };
 
 
-
-
-
+            bindingContext.Result = ModelBindingResult.Success(product);
             return Task.CompletedTask;  
         }
     }
